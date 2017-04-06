613528e1ee9f628a3e749595a88b761832a324dd
Mark Martinez <marksmusic29@gmail.com>
2017-04-06 08:44:16 -0400

    040617

--- assets/js/hotspot.js
+++ assets/js/hotspot.js
@@ -9,20 +9,21 @@ $('#hs-mainbody-wrapper').height(mainBody);
 $('#hs-mainbody-submit').click(function() {
     event.preventDefault();
     //show the main app content
- $('#hs-mainbody-wrapper').addClass('slideOutUp');
- 
-    
+    $('#hs-mainbody-wrapper').addClass('slideOutUp');
+
+
     var show = setTimeout(toggleAppPage, 200);
-    function toggleAppPage(){
+
+    function toggleAppPage() {
         $('#hs-app-container').toggle();
         var resultsContainerMargin = $('header').height();
-        $('#hs-app-content-container').css('margin-top',resultsContainerMargin + 50 + 'px');
+        $('#hs-app-content-container').css('margin-top', resultsContainerMargin + 50 + 'px');
     }
-        
+
     //adds class to the landing page wrapper
     //added class slides out landing page
-   
-   
+
+
 
 });
 var city;
@@ -37,9 +38,10 @@ function getSearch() {
     serchPerent = $(serchPerentDiv);
     //getting the value from search box
     city = serchPerent.find('input').val()
+    type = serchPerent.find('#hs-Type').val()
         //set type in var
     console.log(city);
-    getCityLAndL();
+    getFourData();
 }
 //call maps api
 //get gio location of city
@@ -51,7 +53,7 @@ function getSearch() {
 //function for getting value from serach input
 
 
-function getCityLAndL() {
+function getFourData() {
     $.ajax({
             url: 'https://api.foursquare.com/v2/venues/search?v=20161016',
             medthod: 'GET',
@@ -62,11 +64,8 @@ function getCityLAndL() {
             }
         })
         .done(function(response) {
-            console.log("success");
-            lat = response.response.geocode.feature.geometry.center.lat;
-            lng = response.response.geocode.feature.geometry.center.lng;
-            ll = lat + "," + lng;
-            getTrending();
+            console.log("success");;
+            populateResults(response);
         })
         .fail(function() {
             console.log("error");
@@ -74,60 +73,56 @@ function getCityLAndL() {
 
 }
 
-// use lat & lng to search for trending locations
-function getTrending() {
-    $.ajax({
-            url: 'https://api.foursquare.com/v2/venues/trending?v=20161016',
-            medthod: 'GET',
-            data: {
-                ll: ll,
-                client_id: 'I23VPE32IPM5CTJ0DLH0QD1AUGOMINAG5UVUHK11CZJDUUJC',
-                client_secret: 'DST5ED0CW1XQZN2GY4QD2JLOZY1W5EUSCFC3OFME1ECFLBLI'
-            }
-        })
-        .done(function(response) {
-            console.log("success");
-            console.log(response);
-        populateResults(response);
-        })
-        .fail(function() {
-            console.log("error");
-
-        });
-
-};
-
 
 //----------------------------------------------------Mark
 function populateResults(response) {
-
-    $('#hs-app-content-container').empty();
-    $('#hs-app-content-container').append('<div class"row" id="hs-results-Container"></div>');
+    var venuesObj = response.response.venues;
+    console.log(type);
+    var results = _.orderBy(venuesObj, ['hereNow.count'], ['desc']);
+    console.log(results);
+    var appResultsHtml = $('#hs-place-results').html();
+    appResultsTemp = $(appResultsHtml);
+    console.log(appResultsTemp);
+    $('#hs-results-container').empty().append(appResultsTemp);
     //loop thrue all items
-    for (var i = 0; i < resultLimit; i++) {
-        //get amount of check ins 
-        var here = response.response.venue[i].hereNow.count;
-        console.log(response);
-        //get photo 
-        var photo;
-        //get social id's
-        var twitter;
-        var facebook;
-        //get location
-        var address;
-        //get type of location
-        var type;
-        //set fire or ice
-        if (hereNow > 10) {
-            rating = 'hotimage';
-        } else {
-            rating = 'coldimage';
-        }
-        //append a div with each location info and check in rating
-        $('#hs-results-Container').append('here now'+ here + '   <br>');
+    for (i = 0; i < resultLimit; i++) {
+
+            var appResultHtml = $('#hs-result').html();
+            var appResultTemp = $(appResultHtml);
+            //get amount of check ins 
+            var here = results[i].hereNow.count;
+            //get photo 
+            var photo;
+            var name = results[i].name;
+            //get social id's
+            var twitter;
+            var facebook;
+            //get location
+            var address;
+            //get type of location
+            var cat = results[i].categories[0].name;
+            var venueId = results[i].id;
+            //set fire or ice
+            if (here > 0) {
+                rating = 'hotimage';
+            } else {
+                rating = 'coldimage';
+            }
+            //append a div with each location info and check in rating
+
+
+            console.log(appResultTemp);
+            appResultTemp.addClass('slideInUp ');
+            appResultsTemp.append(appResultTemp);
+            appResultsTemp.attr('venueId', venueId);
+            appResultTemp.find('#name').text(name);
+            appResultTemp.find('#type').text(cat);
+            appResultTemp.find('#rating').text(rating);
+
     }
 
-};
+    $('#hs-results-container').append('<button id="load-more" class="btn">load more results</button>');
+}
 
 //append a div with each location info and check in rating
 //when a location is selected 
