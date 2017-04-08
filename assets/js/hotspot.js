0cae948e3a587bb1d970d208d9eec8b4a9af55b8
Mark Martinez <marksmusic29@gmail.com>
2017-04-07 21:27:31 -0400

    updated all files

--- assets/js/hotspot.js
+++ assets/js/hotspot.js
@@ -12,7 +12,7 @@ $('#hs-mainbody-submit').click(function() {
     $('#hs-mainbody-wrapper').addClass('slideOutUp');
 
 
-    var show = setTimeout(toggleAppPage, 200);
+    var show = setTimeout(toggleAppPage, 1500);
 
     function toggleAppPage() {
         $('#hs-app-container').toggle();
@@ -37,8 +37,8 @@ function getSearch() {
     serchPerentDiv = $(this).parent().parent()[0];
     serchPerent = $(serchPerentDiv);
     //getting the value from search box
-    city = serchPerent.find('input').val()
-    type = serchPerent.find('#hs-Type').val()
+    city = serchPerent.find('input').val();
+    type = 'city';
         //set type in var
     console.log(city);
     getFourData();
@@ -59,6 +59,8 @@ function getFourData() {
             medthod: 'GET',
             data: {
                 near: city,
+                radius: '3000',
+                limit: '100',
                 client_id: 'I23VPE32IPM5CTJ0DLH0QD1AUGOMINAG5UVUHK11CZJDUUJC',
                 client_secret: 'DST5ED0CW1XQZN2GY4QD2JLOZY1W5EUSCFC3OFME1ECFLBLI'
             }
@@ -77,8 +79,19 @@ function getFourData() {
 //----------------------------------------------------Mark
 function populateResults(response) {
     var venuesObj = response.response.venues;
+    var filteredVenues = _.reject(venuesObj, function(venue) {
+        return venue.categories.some(function(category) {
+            if(category.name.toLowerCase().indexOf('city') >= 0 || category.name.toLowerCase().indexOf('bus') >= 0 || category.name.toLowerCase().indexOf('space') >= 0 || category.name.toLowerCase().indexOf('neighborhood') >= 0 || category.name.toLowerCase().indexOf('station') >= 0 || category.name.toLowerCase().indexOf('pharmacy') >= 0 || category.name.toLowerCase().indexOf('store') >= 0 || category.name.toLowerCase().indexOf('bank') >= 0){
+                return true; 
+            }else{
+                return false;
+            }
+
+        });
+    });
     console.log(type);
-    var results = _.orderBy(venuesObj, ['hereNow.count'], ['desc']);
+    console.log(filteredVenues);
+    var results = _.orderBy(filteredVenues, ['hereNow.count'], ['desc']);
     console.log(results);
     var appResultsHtml = $('#hs-place-results').html();
     appResultsTemp = $(appResultsHtml);
@@ -87,43 +100,64 @@ function populateResults(response) {
     //loop thrue all items
     for (i = 0; i < resultLimit; i++) {
 
-            var appResultHtml = $('#hs-result').html();
-            var appResultTemp = $(appResultHtml);
-            //get amount of check ins 
-            var here = results[i].hereNow.count;
-            //get photo 
-            var photo;
-            var name = results[i].name;
-            //get social id's
-            var twitter;
-            var facebook;
-            //get location
-            var address;
-            //get type of location
-            var cat = results[i].categories[0].name;
-            var venueId = results[i].id;
-            //set fire or ice
-            if (here > 0) {
-                rating = 'hotimage';
-            } else {
-                rating = 'coldimage';
-            }
-            //append a div with each location info and check in rating
+        var appResultHtml = $('#hs-result').html();
+        var appResultTemp = $(appResultHtml);
+        //get amount of check ins 
+        var here = results[i].hereNow.count;
+        //get photo 
+        venueId = results[i].id;
+        var photo = getphotos;
+        var name = results[i].name;
+        //get social id's
+        var twitter;
+        var facebook;
+        //get location
+        var address;
+        //get type of location
+        var cat = results[i].categories[0].name;
+        //set fire or ice
+        if (here > 0) {
+            rating = 'hotimage';
+        } else {
+            rating = 'coldimage';
+        }
 
+        //append a div with each location info and check in rating
+        getphotos();
 
-            console.log(appResultTemp);
-            appResultTemp.addClass('slideInUp ');
-            appResultsTemp.append(appResultTemp);
-            appResultsTemp.attr('venueId', venueId);
-            appResultTemp.find('#name').text(name);
-            appResultTemp.find('#type').text(cat);
-            appResultTemp.find('#rating').text(rating);
+        console.log(appResultTemp);
+        appResultsTemp.append(appResultTemp);
+        appResultTemp.addClass('slideInUp ');
+        appResultTemp.attr('venueId', venueId);
+        appResultTemp.find('#hs-place-image').attr('src', photoUrl);
+        appResultTemp.find('#name').text(name);
+        appResultTemp.find('#type').text(cat);
+        appResultTemp.find('#rating').text(rating);
 
     }
 
     $('#hs-results-container').append('<button id="load-more" class="btn">load more results</button>');
 }
 
+function getphotos() {
+
+    $.ajax({
+        url: 'https://api.foursquare.com/v2/venues/' + venueId + '/photos?v=20170404',
+        medthod: 'GET',
+        data: {
+            client_id: 'I23VPE32IPM5CTJ0DLH0QD1AUGOMINAG5UVUHK11CZJDUUJC',
+            client_secret: 'DST5ED0CW1XQZN2GY4QD2JLOZY1W5EUSCFC3OFME1ECFLBLI'
+        },
+        async: false
+    }).done(function(response) {
+        var pre = response.response.photos.items[0].prefix;
+        var suf = response.response.photos.items[0].suffix;
+        var size = '500x500';
+        photoUrl = pre + size + suf;
+        console.log(photoUrl);
+    });
+
+}
 //append a div with each location info and check in rating
 //when a location is selected 
 //empty app content container div
